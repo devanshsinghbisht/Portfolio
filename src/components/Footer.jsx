95610db1bd8004855a4a0de07d3ad34dbbfe66b6
Devansh Bisht <devanshsingh2222@gmail.com>
2023-01-14 15:55:00 +0530

    'commit'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { staggerContainer , fadeIn , zoomIn} from "../utils/motion";
+import { Link } from "react-scroll";
 const  Footer = () => {
   return (
     <>
@@ -11,26 +12,26 @@ const  Footer = () => {
                 </motion.div>
                 <div className="flex flex-col gap-5 h-full md:basis-2/4  basis-2/3 items-center">
                     <div className="flex flex-row py-5 md:gap-20 gap-5">
-                        <motion.div variants={fadeIn('up' , 10  )} transition = {{type : 'spring' , duration : 0.5 , bounce : 0.5}}>                          
-                            <a href="#Project" className="text-slate-300 md:text-[18px] hover:text-white text-[16px] font-Ramabhadra">Projects</a>
-                        </motion.div>
                         <motion.div variants={fadeIn('up' , 10  )} transition = {{type : 'spring' , duration : 0.5 , bounce : 0.5}}>                             
-                            <a href="#Skillset" className="text-slate-300 md:text-[18px] hover:text-white first-letter:text-[16px] font-Ramabhadra">Skillset</a>
+                            <Link to='skillset' className="text-slate-300 md:text-[18px] cursor-pointer hover:text-white first-letter:text-[16px] font-Ramabhadra">Skillset</Link>
                         </motion.div>
                         <motion.div variants={fadeIn('up' , 10  )} transition = {{type : 'spring' , duration : 0.5 , bounce : 0.5}}>                          
-                            <a href="#Contact" className="text-slate-300 md:text-[18px] hover:text-white first-letter:text-[16px] font-Ramabhadra">Contact me</a>
+                            <Link  to='projects' className="text-slate-300 md:text-[18px] cursor-pointer hover:text-white text-[16px] font-Ramabhadra">Projects</Link>
+                        </motion.div>
+                        <motion.div variants={fadeIn('up' , 10  )} transition = {{type : 'spring' , duration : 0.5 , bounce : 0.5}}>                          
+                            <Link to='contact' className="text-slate-300 md:text-[18px] cursor-pointer hover:text-white first-letter:text-[16px] font-Ramabhadra">Contact me</Link>
                         </motion.div>
                     </div>  
                     <motion.span variants={zoomIn()} className="text-white  ">@2023 all rights reserved</motion.span>
                 </div>
-                <div className="flex flex-col basis-1/4 gap-3 h-full py-5   ">
+                <div className="flex flex-col basis-1/4 gap-3 h-full py-5 ">
                     <motion.a variants={fadeIn('up' , 10  )} transition = {{type : 'spring' , duration : 0.5 , bounce : 0.5}} href="https://www.linkedin.com/in/devansh-bisht-b72905199/" target="_blank" className="text-slate-300 md:text-[18px] hover:text-white first-letter:text-[16px] font-serif">Linkedin</motion.a>
                     <motion.a variants={fadeIn('up' , 10  )} transition = {{type : 'spring' , duration : 0.5 , bounce : 0.5}} href="https://www.instagram.com/devansh_singh_bisht/" target="_blank" className="text-slate-300 md:text-[18px] hover:text-white first-letter:text-[16px] font-serif">Instagram</motion.a>
                     <motion.a variants={fadeIn('up' , 10  )} transition = {{type : 'spring' , duration : 0.5 , bounce : 0.5}} href="https://github.com/devanshsinghbisht" target="_blank" className="text-slate-300 md:text-[18px] hover:text-white first-letter:text-[16px] font-serif">Github</motion.a>
                 </div>
             </motion.div>
         </div>
-    </>   
+    </>
   )
 }
 
